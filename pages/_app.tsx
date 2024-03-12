c7b6c45955d162b1f5f9db9c961963c772a81d27
Sebastian <ksiezak.sebastian@gmail.com>
2024-03-12 10:28:32 +0100

    install next/font, add roboto_slab font to the app as default styling clear styling in global.css clear content from index.tsx main page and test global font style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { Roboto_Slab } from "next/font/google";
+
+export const roboto_slab = Roboto_Slab({
+  subsets: ["latin"],
+  variable: "--font-roboto-slab",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${roboto_slab.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
