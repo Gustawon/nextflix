0ade594e4351db09ad9f7860dc0c73b6f47f0a5a
Sebastian <ksiezak.sebastian@gmail.com>
2024-03-12 11:00:29 +0100

    Add Head to index Page, UPdate favicon.ico to Netflix icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import { roboto_slab } from "./_app";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center gap-10 text-7xl pt-40`}
-    >
-      <div className={`${roboto_slab.className}`}>NEXTFLIX - div styling</div>
-      <div>NEXTFLIX - default app styling</div>
-    </main>
+    <>
+      <Head>
+        <title>Nextflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main
+        className={`flex min-h-screen flex-col items-center gap-10 text-7xl pt-40`}
+      >
+        <div className={`${roboto_slab.className}`}>NEXTFLIX - div styling</div>
+        <div>NEXTFLIX - default app styling</div>
+      </main>
+    </>
   );
 }
